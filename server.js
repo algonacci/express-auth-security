ba5314e0f950f47d3c5964366d960667a244b786
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2022-12-23 22:42:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/secret", (req, res) => {
+  return res.send("Your personal secret value is 42!");
+});
+
 app.listen(PORT, () => {
   console.log(`🚀 [SERVER] is running on port http://localhost:${PORT}`);
 });
